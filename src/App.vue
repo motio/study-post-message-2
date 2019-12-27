27b32de7d32231149f8173acaef7d1bd4b70fad8
Keita Sakamoto <keita.sakamoto@dena.com>
2019-12-27 14:59:15 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>送信側</h1>
+    <button @click="submit">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: '送られたテキスト',
+    };
+  },
+  methods: {
+    submit() {
+      window.parent.postMessage(this.msg, 'http://localhost:5000');
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
